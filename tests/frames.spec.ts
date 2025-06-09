7c4e093175043a90647bd19c75933f7f31c391ac
JRM <jrmandhyannee24@gmail.com>
2025-06-09 23:02:40 +0530

    cleanup

--- tests/frames.spec.ts
+++ tests/frames.spec.ts
@@ -9,9 +9,7 @@ test("Frame Handling Using Page.Frame()", async ({ page }) => {
   const allFrames = page.frames();
   const allFramesCount = allFrames.length;
   console.log("Total Frames Count is " + allFramesCount);
-  const frame = page.frame({
-    url: "https://ui.vision/demo/webtest/frames/frame_1.html",
-  });
+  const frame = page.frame({ url: "https://ui.vision/demo/webtest/frames/frame_1.html" });
   await frame.locator('input[name="mytext1"]').fill("Playwright");
 });
 
@@ -21,15 +19,10 @@ test("Frame Handling Using Page.FrameLocator()", async ({ page }) => {
 });
 
 test("Nested Frame Handling - Fill Form present on iframe inside frame", async ({ page }) => {
-
-  const frame3 = page.frame({
-    url: "https://ui.vision/demo/webtest/frames/frame_3.html",
-  });
+  const frame3 = page.frame({ url: "https://ui.vision/demo/webtest/frames/frame_3.html" });
   const childFrames = frame3.childFrames();
   console.log("Number of Child Frames " + childFrames.length);
-  await childFrames[0]
-    .getByRole("radio", { name: "Hi, I am the UI.Vision IDE" })
-    .click();
+  await childFrames[0].getByRole("radio", { name: "Hi, I am the UI.Vision IDE" }).click();
   const checkboxNames = [
     "Web Testing",
     "Form Autofilling",
@@ -40,25 +33,12 @@ test("Nested Frame Handling - Fill Form present on iframe inside frame", async (
   }
   await childFrames[0].getByText("Choose").click({ force: true });
   await page.waitForTimeout(1000);
-  await childFrames[0]
-    .getByRole("option", { name: "Yes" })
-    .locator("span")
-    .click({ force: true });
-  await childFrames[0]
-    .getByRole("button", { name: "Next" })
-    .click({ force: true });
-  await childFrames[0]
-    .getByRole("textbox", { name: "Enter a short text" })
-    .waitFor();
-  await childFrames[0]
-    .getByRole("textbox", { name: "Enter a short text" })
-    .fill("Short");
-  await childFrames[0]
-    .getByRole("textbox", { name: "Enter a long answer" })
-    .fill("Long");
-  await childFrames[0]
-    .getByRole("button", { name: "Submit" })
-    .click({ force: true });
+  await childFrames[0].getByRole("option", { name: "Yes" }).locator("span").click({ force: true });
+  await childFrames[0].getByRole("button", { name: "Next" }).click({ force: true });
+  await childFrames[0].getByRole("textbox", { name: "Enter a short text" }).waitFor();
+  await childFrames[0].getByRole("textbox", { name: "Enter a short text" }).fill("Short");
+  await childFrames[0].getByRole("textbox", { name: "Enter a long answer" }).fill("Long");
+  await childFrames[0].getByRole("button", { name: "Submit" }).click({ force: true });
   await expect(childFrames[0].locator("div.vHW8K")).toContainText(
     "Thank you for testing the UI.Vision RPA software! As this is just a test form, all submitted data is automatically deleted"
   );
